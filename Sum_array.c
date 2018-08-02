5575a880b38a61556503a17e39948315dbdf7b6e
dinoperez3 <tamfudivine3@gmail.com>
2018-08-02 22:15:19 +0100

    sum of array

---
+++ Sum_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>   
+    int sumArray(int arr[], int n);
+     int main(int argc, char const *argv[])
+  { 
+    int myarray[] = {1,2,3,4,5};
+    int arraysize = sizeof(myArray)/sizeof(myArray[0]);
+     int n = sizeof(a)/sizeof(a[0]);
+     int results = sumArray(myArray,arraysize);
+        printf("%d", result);
+    }
+ int array(int A[], int n){
+        int sum;
+         int i;
+       for(i=0; i<n; i+1){
+          sum= sum + A[i];}
+ return sum;
+}
+
+
+
+
